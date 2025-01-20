a700d809cf0e9e0dbf450c28ab30148e757bfe64
mfarhannr <muhammad755262@smk.belajar.id>
2025-01-20 14:55:49 +0700

    clear

--- src/app.js
+++ src/app.js
@@ -1,22 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-
-dotenv.config();
-
+const financeRoutes = require("./routes/financeRoutes");
+const PORT = process.env.PORT || 3000;
+const userRoutes = require("./routes/userRoutes");
 const app = express();
 
+dotenv.config();
 app.use(express.json());
-
-const PORT = process.env.PORT || 3000;
-
-const userRoutes = require('./routes/userRoutes');
-// const financeRoutes = require('./routes/financeRoutes');
-
 app.use('/api/users', userRoutes);
-// app.use('/api/finance', financeRoutes);
-
-
+app.use("/api/finances", financeRoutes);
 app.listen(PORT, () => console.log(`Server berjalan di port ${PORT}`));
 
 connectDB();
