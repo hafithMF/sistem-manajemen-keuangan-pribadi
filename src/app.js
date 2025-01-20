d5af71d64caeafde722df6204e926123a137d6ba
mfarhannr <muhammad755262@smk.belajar.id>
2025-01-20 14:38:44 +0700

    register and login

--- src/app.js
+++ src/app.js
@@ -8,7 +8,14 @@ const app = express();
 
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+
+const userRoutes = require('./routes/userRoutes');
+// const financeRoutes = require('./routes/financeRoutes');
+
+app.use('/api/users', userRoutes);
+// app.use('/api/finance', financeRoutes);
+
 
 app.listen(PORT, () => console.log(`Server berjalan di port ${PORT}`));
 
