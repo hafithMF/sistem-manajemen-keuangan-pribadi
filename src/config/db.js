b30368c6e9ce1ea64b6602dfae9b6228f40b6a1f
mfarhannr <muhammad755262@smk.belajar.id>
2025-02-04 13:02:48 +0700

    terhunbung ke MongoDb Atlas

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Terkoneksi: ${conn.connection.host}`);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000, 
+    });
+    console.log(`✅ MongoDB Terkoneksi: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
+    console.error(`❌ MongoDB Connection Error: ${error.message}`);
+    process.exit(1); 
   }
 };
 
